0c8694a4cd5f447c980bb3e51d5d2a31f057f530
Ricardo Altmann <tenaltmann@gmail.com>
2026-03-25 22:04:43 -0300

    Editando tags e criando as funções das rotas

--- auth_routes.py
+++ auth_routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter       # importando roteador
 
-auth_router = APIRouter(prefix= "/auth")    # definindo prefixo padrao da rota
+auth_router = APIRouter(prefix= "/auth", tags=["auth"])    # definindo prefixo padrao da rota
                                             # Ex:    https://dominio/AUTH/caminho definido a partir daqui
 
 
--- order_routes.py
+++ order_routes.py
@@ -1,4 +1,17 @@
 from fastapi import APIRouter       # importando roteador
 
-order_router = APIRouter(prefix= "/order")    # definindo prefixo padrao da rota
+order_router = APIRouter(prefix= "/order", tags=["pedidos"])    # definindo prefixo padrao da rota
                                               # Ex:    https://dominio/ORDER/caminho definido a partir daqui
+
+#   PARA CRIAÇÃO DE ROTAS
+    # Estrutura da rota:
+        # @ = decorator (usado para definir que é uma rota) | order_router = variável que atribuimos o APIRouter | .get = metodo aser utilizado (get, post, put,etc ..) | ("/") = caminho do endpoint  
+
+@order_router.get("/")
+async def pedidos():
+        
+        """
+        Essa é a rota padrão de pedidos do nosso sistema
+
+        """
+        return {"mensagem":"Você acessou a rota de pedidos"}
